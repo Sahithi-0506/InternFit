44033d8aa8bbd93be035a819aa76f7053fcbed73
Sahithi-0506 <24b01a12e8@svecw.edu.in>
2026-05-09 15:00:16 +0530

    Initial INTERN FIT setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("INTERN FIT Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
