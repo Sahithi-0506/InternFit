2ed5c22b42ae90d3f9dcb48e230e889966a7c331
Sahithi-0506 <24b01a12e8@svecw.edu.in>
2026-05-17 12:18:43 +0530

    Updated Homepage better UI

--- frontend/src/pages/StudentRegister.jsx
+++ frontend/src/pages/StudentRegister.jsx
@@ -11,9 +11,7 @@ function StudentRegister() {
     password: "",
     year_of_study: "",
     branch: "",
-    skills: "",
-    projects: "",
-    interests: "",
+    
   });
 
   const handleChange = (e) => {
@@ -42,10 +40,7 @@ function StudentRegister() {
         <Input name="password" type="password" placeholder="Password" onChange={handleChange} />
         <Input name="year_of_study" placeholder="Year of Study" onChange={handleChange} />
         <Input name="branch" placeholder="Branch" onChange={handleChange} />
-        <Input name="skills" placeholder="Skills e.g. HTML,CSS,JavaScript" onChange={handleChange} />
-        <Input name="projects" placeholder="Projects" onChange={handleChange} />
-        <Input name="interests" placeholder="Interests" onChange={handleChange} />
-
+        
         <button className="w-full mt-5 bg-green-600 text-white py-3 rounded-xl hover:bg-green-700">
           Register
         </button>
